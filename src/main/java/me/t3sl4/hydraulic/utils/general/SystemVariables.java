c10ef526be6b70417ca1d2214ccb9c8e8aac8c46
Halil İbrahim Direktör <hidirektor@gmail.com>
2024-11-23 11:01:54 +0300

    windows Release

--- src/main/java/me/t3sl4/hydraulic/utils/general/SystemVariables.java
+++ src/main/java/me/t3sl4/hydraulic/utils/general/SystemVariables.java
@@ -9,7 +9,7 @@ public class SystemVariables {
     public static boolean offlineMode = false;
     public static String otpSentTime;
 
-    private static final String CURRENT_VERSION = "v3.4.9";
+    private static final String CURRENT_VERSION = "v3.5.0";
 
     public static String BASE_URL = "https://ondergrup.hidirektor.com.tr/api/v2";
     public static String RELEASE_URL = "https://github.com/hidirektor/ondergrup-hydraulic-tool/releases";
